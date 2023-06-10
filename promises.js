9f41060fb4419ce78e6f02bf692114c9095ca761
datalyst1127 <96359837+datalyst1127@users.noreply.github.com>
2023-06-10 11:05:35 -0700

    worked on q 1

--- promises.js
+++ promises.js
@@ -1,19 +1,23 @@
 // Challenge 1: Delayed Promise
 function delayedPromise(message, delay) {
-    // TODO: Write code to create a promise that resolves with the message after the specified delay using setTimeout
-  }
-  
-  // Challenge 2: Random Number Promise
-  function randomPromise() {
-    // TODO: Write code to create a promise that generates a random number between 1 and 10
-    // Resolve the promise with the random number if it is even, and reject it with the message "Odd number" if it is odd
-  }
-  
-  // Challenge 3: Fetch Data Promise
-  function fetchData(url) {
-    // TODO: Write code to create a promise that fetches data from the given URL (simulated async request)
-    // After a 2-second delay, resolve the promise with a sample data object
-  }
-  
-  module.exports = { delayedPromise, randomPromise, fetchData };
-  
+  // TODO: Write code to create a promise that resolves with the message after the specified delay using setTimeout
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(message);
+    }, delay);
+  });
+}
+
+// Challenge 2: Random Number Promise
+function randomPromise() {
+  // TODO: Write code to create a promise that generates a random number between 1 and 10
+  // Resolve the promise with the random number if it is even, and reject it with the message "Odd number" if it is odd
+}
+
+// Challenge 3: Fetch Data Promise
+function fetchData(url) {
+  // TODO: Write code to create a promise that fetches data from the given URL (simulated async request)
+  // After a 2-second delay, resolve the promise with a sample data object
+}
+
+module.exports = { delayedPromise, randomPromise, fetchData };
