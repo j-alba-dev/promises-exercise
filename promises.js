31d07f56070e1e0e45e106b27f518234ca79c9a3
datalyst1127 <96359837+datalyst1127@users.noreply.github.com>
2023-06-11 14:14:34 -0700

    finished last two q's

--- promises.js
+++ promises.js
@@ -12,12 +12,35 @@ function delayedPromise(message, delay) {
 function randomPromise() {
   // TODO: Write code to create a promise that generates a random number between 1 and 10
   // Resolve the promise with the random number if it is even, and reject it with the message "Odd number" if it is odd
+  return new Promise((resolve, reject) => {
+    const num = Math.floor(Math.random() * 10) + 1;
+    if (num % 2 === 0) {
+      resolve(num);
+    } else {
+      reject("Odd number");
+    }
+  });
 }
+
+// console.log(randomPromise());
 
 // Challenge 3: Fetch Data Promise
 function fetchData(url) {
   // TODO: Write code to create a promise that fetches data from the given URL (simulated async request)
   // After a 2-second delay, resolve the promise with a sample data object
+  return new Promise((resolve, reject) => {
+    setTimeout(
+      (func = () => {
+        const data = {
+          data: "Sample data object",
+        };
+        resolve(data);
+      }),
+      2000
+    );
+  });
 }
+
+console.log(fetchData);
 
 module.exports = { delayedPromise, randomPromise, fetchData };
--- promisesSolution.js
+++ promisesSolution.js
@@ -1,38 +1,37 @@
 // Challenge 1: Delayed Promise
 function delayedPromise(message, delay) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve(message);
-      }, delay);
-    });
-  }
-  
-  // Challenge 2: Random Number Promise
-  function randomPromise() {
-    return new Promise((resolve, reject) => {
-      const randomNumber = Math.floor(Math.random() * 10) + 1; // Random number between 1 and 10
-      if (randomNumber % 2 === 0) {
-        resolve(randomNumber);
-      } else {
-        reject('Odd number');
-      }
-    });
-  }
-  
-  // Challenge 3: Fetch Data Promise
-  function fetchData(url) {
-    return new Promise((resolve, reject) => {
-      // Simulating an asynchronous request
-      setTimeout(() => {
-        const data = {
-          id: 1,
-          name: 'John Doe',
-          email: 'johndoe@example.com'
-        };
-        resolve(data);
-      }, 2000);
-    });
-  }
-  
-  module.exports = { delayedPromise, randomPromise, fetchData };
-  
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(message);
+    }, delay);
+  });
+}
+
+// Challenge 2: Random Number Promise
+function randomPromise() {
+  return new Promise((resolve, reject) => {
+    const randomNumber = Math.floor(Math.random() * 10) + 1; // Random number between 1 and 10
+    if (randomNumber % 2 === 0) {
+      resolve(randomNumber);
+    } else {
+      reject("Odd number");
+    }
+  });
+}
+
+// Challenge 3: Fetch Data Promise
+function fetchData(url) {
+  return new Promise((resolve, reject) => {
+    // Simulating an asynchronous request
+    setTimeout(() => {
+      const data = {
+        id: 1,
+        name: "John Doe",
+        email: "johndoe@example.com",
+      };
+      resolve(data);
+    }, 2000);
+  });
+}
+
+module.exports = { delayedPromise, randomPromise, fetchData };
